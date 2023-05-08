301da6afd25a81bdce3d920e3922ad4b58e2921f
Pryesh Gohil <45434154+pryeshgohil@users.noreply.github.com>
2023-05-08 21:20:46 +0100

    added javascript

---
+++ api/HttpTrigger1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Company.Function
+{
+    public static class HttpTrigger1
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        [FunctionName("HttpTrigger1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadLineAsync();
+
+            StringContent content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://pryeshgohil-ddt11f1.azurewebsites.net/api/HttpTrigger1?code=vve_phWnLlP_S7TM1uF1ZIgTZxeYu2hc2ROSBKot_Gg_AzFutieeKw==", content);
+
+            var responseMessage = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"--> Response: {responseMessage}");
+
+            return new OkObjectResult("Payload Received");
+
+        }
+    }
+}
